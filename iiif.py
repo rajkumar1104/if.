c12b87cb50f5e0aa10dfcaf3cfc75da86393f7da
Rajkumar.Kampelly <kampellyrajkumar3@gmail.com>
2025-01-18 14:28:41 +0530

    Add files via upload

---
+++ iiif.py
@@ -0,0 +1,22 @@
+weather=input("how is weather condition? ")
+
+if weather=='hot':
+    print("i will drink beer ")
+
+elif weather=='cool':
+    print("i will drink whisky ")
+
+elif weather=='warm':
+    print("i will drink rum ") 
+
+elif weather=='rainy':
+    print("i will drink rum with hot bajji ") 
+
+else:
+    print("i will drink water")   
+
+
+
+
+
+    
