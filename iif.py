a71321cd5b9a56020e99fa1bad8f0425e8d63798
Rajkumar.Kampelly <kampellyrajkumar3@gmail.com>
2025-01-18 14:29:45 +0530

    Add files via upload

---
+++ iif.py
@@ -0,0 +1,17 @@
+game=input("enter your game: ")
+
+if game=='cricket':
+ print("i will come to ground")
+
+elif game=='volley ball':
+  print("i wont come to ground")
+
+elif game=='kabbaddi':
+   print("i will come to ground")
+
+elif game=='kho-kho':
+   print("i wont come to ground")
+
+else:
+  print("sorry ,there is no such type of game")
+
