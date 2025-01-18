08c52c196d557efbf6b91a8656ee74014cd8312a
Rajkumar.Kampelly <kampellyrajkumar3@gmail.com>
2025-01-18 14:25:59 +0530

    Add files via upload
    this is a simple if conditions

---
+++ iiiif.py
@@ -0,0 +1,22 @@
+day=input("select a day: ")
+
+if day=="monday":
+    print("today is back workout ")
+
+elif day=="tuesday":
+    print("today is shoulders ")    
+
+elif day=="wednesday":
+    print("today is tricep ")    
+
+elif day=="thursday":
+    print("today is bicep ")    
+
+elif day=="friday":
+    print("today is abs ")    
+
+elif day=="saturday":
+    print("today is legs ")    
+else:
+    print("today is rest")   
+            
